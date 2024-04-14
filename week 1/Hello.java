a08759aeaf353fb549a1b89154ab7288dda842c6
dhakalsundar <dhakalsundar440@gmail.com>
2024-04-14 12:48:35 +0545

    week 1

--- week 1/Hello.java
+++ week 1/Hello.java
@@ -1,8 +1,87 @@
 
-public class Hello{
+/*public class Hello{
     public static void main(String[] args) {
         /* 1. Write a program that prints Hello World! to the console. Save the program as Hello.java.*/
-         System.out.println("Hello World!");
-    }
+        /*  System.out.println("Hello World!");*/
 
+        /*  2. Extend/Modify the above program to print two additional lines:
+
+This is my first program
+
+I am on module ST4003CEM
+
+System.out.print (not println) and produces the same output.*/
+
+/*public class Hello {
+    public static void main(String[] args) {
+        System.out.println("This is my first program!\n");
+        System.out.println("I am on module ST4003CEM");
+ } 
+}    */
+
+
+/*3. Write a program that prints the following output:
+
+A "quoted" String is
+
+'much' better if you learn
+
+the rules of "escape sequences."
+
+Also, "" represents an empty
+
+String. Don't forget: use \"
+
+instead of " !
+
+'' is not the same as " */
+
+/*public class Hello{
+    public static void main(String[] args) {
+        System.out.println("A \"quoted\" String is");
+        System.out.println("'much' better if you learn");
+        System.out.println("the rule of \"escape sequences.\"");
+        System.out.println("Also, \"\"represent an empty");
+        System.out.println("String.\"Don't\"forget: use\\\"");
+        System.out.println("insted of \"!''is not the same as \\");
+
+    }
+}*/
+
+/*4. WAP that prints the following pattern:
+
+*
+
+**
+
+***
+
+**** */
+
+/*public class Hello{
+    public static void main(String[]args){
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
 }
+
+/*5. WAP prints the following pattern:
+
+*********
+
+*********
+
+*********
+
+********* */
+
+/*public class Hello{
+    public static void main(String[]args){
+        System.out.println("******");
+        System.out.println("******");
+        System.out.println("******");
+        System.out.println("******");
+    }
+}*/
