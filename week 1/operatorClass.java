0e6c86ee8681116e3298b458f1658a77b4ef58e1
dhakalsundar <dhakalsundar440@gmail.com>
2024-04-16 14:54:51 +0545

    week 2

--- week 1/operatorClass.java
+++ week 1/operatorClass.java
@@ -19,7 +19,21 @@ public class operatorClass {
         System.out.println("Assignment with += " + (aNumber1+=10));
         aNumber2 -= 30; // same as aNumber2 = aNumber2 -30;
         System.out.println("Assignment with -=" + aNumber2);
+
+         /* Relational Operation */
+         int rNumber1 = 10, rNumber2 = 20;
+         System.out.println("rNumber1 equal to rNumber2?" + (rNumber1 == rNumber2));
+         System.out.println("rNumber1 greater than rNumber2?" + (rNumber1 > rNumber2));
+         System.out.println("rNumber1 lessthan or equal to rNumber2?" + (rNumber1 <= rNumber2));
+         System.out.println("rNumber1 notequal to rNumber2?" + (rNumber1 != rNumber2));
         
+         /* Logical Operation */
+         int lNumber1 = 10, lNumber2 = 12;
+         boolean lExpression1 = lNumber1 == lNumber2;
+         boolean lExpression2 = lNumber1 <= lNumber2;
+         System.out.println("And operation uses &&" + (lExpression1 && lExpression2));
+         System.out.println("Or operation uses ||" + (lExpression1 || lExpression2));
+         System.out.println("Not operator uses!" + !lExpression1);
 
     }
     
