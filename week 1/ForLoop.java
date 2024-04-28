035b4af5c2c8b42fd607a16ae16ceea2f38cc919
dhakalsundar <dhakalsundar440@gmail.com>
2024-04-28 12:29:56 +0545

    Task

--- week 1/ForLoop.java
+++ week 1/ForLoop.java
@@ -11,31 +11,9 @@ public class ForLoop {
 
         }
         /* Make the loop table for this */
-        /* Task
-         * Make a multiplication table of 2
-         * print like the following
-         * 2*1=2
-         * 2*2=4
-         * .
-         * .
-         * 2*10=20
-         * note: inilitalize 1
-         * condition <=10 or < 11
-         * Increment ++
-         */
-        
-            
-                int number = 2;
-                int max = 10; 
-        
-                for (int i = 1; i <= max; i++) {
-                    System.out.println(number + " * " + i + " = " + (number * i));
-                }
-            }
-        }
+       
 
+    }
 
     
-
-    
-
+}
