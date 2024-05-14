0a6b06b38fefa5496bb98184377dfc138b4e7294
dhakalsundar <dhakalsundar440@gmail.com>
2024-05-14 15:39:50 +0545

    practice

--- week1/week1/ArrayListLoop.java
+++ week1/week1/ArrayListLoop.java
@@ -1,5 +1,7 @@
 package week1;
+import java.util.Arrays;
 import java.util.ArrayList;
+
 public class ArrayListLoop {
     public static void main(String[] args) {
         /* import Arraylist from java.util */
@@ -23,8 +25,30 @@ public class ArrayListLoop {
         /* using foreach in java, iterate with element/ actual value */
         for (String element : names) {
             System.out.println(element);
-            
+
     }
-    
+     /*Task
+         * make ande arraylist animals
+         * ["Cat","Tiger","Dog","Elephant"]
+         * make two more arraylist called wildanimal ,domestic animal
+         * loop the animals
+         * if the element/value is "Cat", 'Dog" add in domestic animal
+         * same for 'Tiger AND "elephant'
+         * print domestic animal and widl animal
+         *
+         */
+        ArrayList <String> animals = new ArrayList<String>(Arrays.asList("Cat","Tiger","Dog","Elephant"));
+        ArrayList<String> wildanimal = new ArrayList<>();
+        ArrayList<String> domesticanimal = new ArrayList<>();
+        for(String animal : animals) {
+            if(animal == "Cat" || animal == "Dog") {
+                domesticanimal.add(animal);
+        }else{
+            wildanimal.add(animal);
+        }
+
+System.out.println("Wild animal: "+ wildanimal);
+System.out.println("Domestic animal: "+ domesticanimal);
 }
+    }
 }
